d0bd88e7617b086c3e9cbb542629774a91a032d8
Shaylin-UCT <chtsha042@myuct.ac.za>
2023-09-14 20:08:58 +0200

    weights added

---
+++ MedFID/FIDTestsCeleb.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+import csv
+import FeatureExtraction
+class FIDTests:
+
+    def __init__(self):
+        self.FIDclass = FeatureExtraction.FeatureExtractionClass()
+
+    def calculateFID(self,image_folder, originalImages):
+        #src_folder = "ImagesforResearch\\FIDConfirmationImages\\CelebA_Runs\\CelebA"
+        
+        return self.FIDclass.getFIDs(originalImages, image_folder)
+        #for root, _, files in os.walk(src_folder):
+
+    def writeToCSV(self,filename,lists):
+        with open(filename, mode = "w", newline="") as file:
+            writer = csv.writer(file)
+            writer.writerows(lists)
+
+    def processFID(self):
+        #runProcesses()
+        #return
+        
+        folders = ["blur", "noise", "saltandpepper", "blocks"]
+        for folder in folders:
+            #folder = "blocks"
+            image_folder = f"/mnt/lustre/users/schetty1/ImagesforResearch/FIDConfirmationImages/CelebAwithDistortions/{folder}"
+            print("Working in:", image_folder)
+            csvData = []
+            headers = ["Iteration", "FID"]
+            csvData.append(headers)
+            for root, _, files in os.walk(image_folder):
+                if root == image_folder:
+                    #Skip main folder as we're just concerned with the subdirectories in the main folder
+                    continue
+                print(root)
+                x = root.rfind("/") #extracts the parameter part of the path
+                param = root[x+1:]
+                src_folder = "/mnt/lustre/users/schetty1/ImagesforResearch/FIDConfirmationImages/CelebAwithDistortions/CelebA"
+                fid = self.calculateFID(root, originalImages=src_folder)
+                print(param)
+                #fid = 0
+                print(root, "--->", fid)
+                temp = [param, fid]
+                csvData.append(temp)
+            #return
+            print(csvData)
+            self.writeToCSV(f"/home/schetty1/MedFIDAttempt2/ParameterFIDScores/FineTunedCeleb/{folder}.csv", csvData)
+        #calculateFIDs(image_folder=image_folder)
+        
+def main():
+    cls = FIDTests()
+    cls.processFID()
+
+if __name__ == "__main__":
+    main()
